fa17f836f73832c2917cc5ee81de23ad8013e7ae
Fabio Sangregorio <fabio.sangregorio@gmail.com>
2019-05-28 20:59:05 +0200

    refactor config

--- telereddit/config.py
+++ telereddit/config.py
@@ -1,19 +1,20 @@
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
 
+def _edit_keyboard(edit_text):
+    return InlineKeyboardMarkup([[
+        InlineKeyboardButton(text="✕", callback_data="delete"),
+        InlineKeyboardButton(text=edit_text, callback_data="edit"),
+        InlineKeyboardButton(text="✓", callback_data="confirm")
+    ]])
+
+
 MAX_POST_LENGTH = 500
 MAX_TITLE_LENGTH = 200
 MAX_TRIES = 4
-EDIT_KEYBOARD = InlineKeyboardMarkup([[
-    InlineKeyboardButton(text="✕", callback_data="delete"),
-    InlineKeyboardButton(text="↻", callback_data="edit"),
-    InlineKeyboardButton(text="✓", callback_data="confirm")
-]])
-EDIT_FAILED_KEYBOARD = InlineKeyboardMarkup([[
-    InlineKeyboardButton(text="✕", callback_data="delete"),
-    InlineKeyboardButton(text="Retry ↻", callback_data="edit"),
-    InlineKeyboardButton(text="✓", callback_data="confirm")
-]])
+
+EDIT_KEYBOARD = _edit_keyboard('↻')
+EDIT_FAILED_KEYBOARD = _edit_keyboard('Retry ↻')
 CONFIRMED_KEYBOARD = InlineKeyboardMarkup([[
     InlineKeyboardButton(text="Show another one", callback_data="more")
 ]])
