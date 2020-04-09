11380c1f219735973b8be092a275375d9fd592e2
Fabio <fabio.sangregorio@gmail.com>
2020-04-09 18:52:48 +0200

    Add tests

--- telereddit/tests/test_reddit.py
+++ telereddit/tests/test_reddit.py
@@ -1,49 +1,70 @@
 import unittest
+from unittest.mock import patch
 import reddit
+import json
+import pathlib
+
+from exceptions import SubredditDoesntExistError, SubredditPrivateError
 
 from content_type import ContentType
 
 
 class TestReddit(unittest.TestCase):
-    def test_get_json(self):
-        # subreddit that doesn't exist
-        json, _ = reddit._get_json('r/n_o_t_a_n_a_m_e_i_h_ox_p_e')
-        self.assertEqual(json, None)
+    @patch('reddit.requests.get')
+    def test_get_json_404(self, mock_get):
+        mock_get.return_value.ok = False
+        mock_get.return_value.json = lambda: {"message": "Not Found", "error": 404}
+        mock_get.return_value.status_code = 404
+        self.assertRaises(SubredditDoesntExistError,
+                          reddit._get_json,
+                          'https://reddit.com/r/n_o_t_a_n_a_m_e_i_h_ox_p_e/random')
 
-        # private subreddit
-        json, _ = reddit._get_json('r/CenturyClub/')
-        self.assertEqual(json, None)
+    @patch('reddit.requests.get')
+    def test_get_json_private(self, mock_get):
+        mock_get.return_value.ok = False
+        mock_get.return_value.json = lambda: {"reason": "private", "message": "Forbidden", "error": 403}
+        mock_get.return_value.status_code = 403
+        self.assertRaises(SubredditPrivateError,
+                          reddit._get_json,
+                          'https://reddit.com/r/n_o_t_a_n_a_m_e_i_h_ox_p_e/random')
 
-        # valid subreddit
-        json, _ = reddit._get_json('r/funny')
-        self.assertNotEqual(json, None)
+    @patch('reddit.requests.get')
+    def test_get_json_valid(self, mock_get):
+        with open(pathlib.Path(__file__).parent / 'json/r-funny-my_weather_app_nailed_it_today.json') as f:
+            mock_json = json.load(f)
+        mock_get.return_value.ok = True
+        mock_get.return_value.json = lambda: mock_json
+        mock_get.return_value.status_code = 200
 
-    def test_get_post(self):
-        # text post
-        post, status, err_msg = reddit.get_post('r/showerthoughts')
-        self.assertEqual(status, 'success')
-        self.assertIsNone(err_msg)
-        self.assertEqual(post.subreddit.lower(), 'r/showerthoughts')
-        self.assertIsNotNone(post.title)
-        self.assertIn('https://www.reddit.com/r/showerthoughts', post.get_msg().lower())
-        self.assertEqual(post.get_type(), ContentType.TEXT)
+        json_data = reddit._get_json('https://www.reddit.com/r/funny/comments/fxuefa/my_weather_app_nailed_it_today.json')
+        self.assertEqual(json_data, mock_json[0])
 
-        # photo post
-        post, status, err_msg = reddit.get_post('r/pics')
-        self.assertEqual(status, 'success')
-        self.assertIsNone(err_msg)
-        self.assertEqual(post.subreddit.lower(), 'r/pics')
-        self.assertIsNotNone(post.title)
-        self.assertIn('https://www.reddit.com/r/pics', post.get_msg().lower())
-        self.assertIsNotNone(post.media.url)
-        self.assertEqual(post.get_type(), ContentType.PHOTO)
+    # def test_get_post(self):
+    #     # text post
+    #     post, status, err_msg = reddit.get_post('r/showerthoughts')
+    #     self.assertEqual(status, 'success')
+    #     self.assertIsNone(err_msg)
+    #     self.assertEqual(post.subreddit.lower(), 'r/showerthoughts')
+    #     self.assertIsNotNone(post.title)
+    #     self.assertIn('https://www.reddit.com/r/showerthoughts', post.get_msg().lower())
+    #     self.assertEqual(post.get_type(), ContentType.TEXT)
 
-        # crosspost
-        post, status, err_msg = reddit.get_post(post_url='https://www.reddit.com/r/bicycling/comments/aevkgj/finally_know_how_to_crosspost/')
-        self.assertEqual(status, 'success')
-        self.assertIsNone(err_msg)
-        self.assertEqual(post.subreddit.lower(), 'r/bicycling')
-        self.assertIsNotNone(post.title)
-        self.assertIn('https://www.reddit.com/r/bicycling', post.get_msg().lower())
-        self.assertIsNotNone(post.media.url)
-        self.assertEqual(post.get_type(), ContentType.PHOTO)
+    #     # photo post
+    #     post, status, err_msg = reddit.get_post('r/pics')
+    #     self.assertEqual(status, 'success')
+    #     self.assertIsNone(err_msg)
+    #     self.assertEqual(post.subreddit.lower(), 'r/pics')
+    #     self.assertIsNotNone(post.title)
+    #     self.assertIn('https://www.reddit.com/r/pics', post.get_msg().lower())
+    #     self.assertIsNotNone(post.media.url)
+    #     self.assertEqual(post.get_type(), ContentType.PHOTO)
+
+    #     # crosspost
+    #     post, status, err_msg = reddit.get_post(post_url='https://www.reddit.com/r/bicycling/comments/aevkgj/finally_know_how_to_crosspost/')
+    #     self.assertEqual(status, 'success')
+    #     self.assertIsNone(err_msg)
+    #     self.assertEqual(post.subreddit.lower(), 'r/bicycling')
+    #     self.assertIsNotNone(post.title)
+    #     self.assertIn('https://www.reddit.com/r/bicycling', post.get_msg().lower())
+    #     self.assertIsNotNone(post.media.url)
+    #     self.assertEqual(post.get_type(), ContentType.PHOTO)
--- telereddit/tests/test_services.py
+++ telereddit/tests/test_services.py
@@ -2,77 +2,90 @@ import unittest
 from services.services_wrapper import ServicesWrapper
 from content_type import ContentType
 
+from parameterized import parameterized, param
+
 
 class TestServices(unittest.TestCase):
-    def test_youtube(self):
+    @parameterized.expand([
         # youtube link (attribution link)
-        json = {
-            "media": {
-                "oembed": {
-                    "url": "https://www.youtube.com/watch?v=DJxchZ7qAzE"
-                }
-            }
-        }
-        media = ServicesWrapper.get_media(
-            "https://www.youtube.com/attribution_link?a=o3Cq80oOnoc&u=%2Fwatch%3Fv%3D3OSc_psp4k0%26feature%3Dshare",
-            json)
-        self.assertEqual(media.url, "https://www.youtube.com/watch?v=DJxchZ7qAzE")
-        self.assertEqual(media.type, ContentType.YOUTUBE)
-
+        param(
+            url="https://www.youtube.com/attribution_link?a=o3Cq80oOnoc&u=%2Fwatch%3Fv%3D3OSc_psp4k0%26feature%3Dshare",
+            json={"media": {"oembed": {"url": "https://www.youtube.com/watch?v=DJxchZ7qAzE"}}},
+            expected_url="https://www.youtube.com/watch?v=DJxchZ7qAzE"
+        ),
         # youtube link (youtube.com)
-        json = {
-            "media": {
-                "oembed": {}
-            }
-        }
-        media = ServicesWrapper.get_media("https://www.youtube.com/watch?v=DJxchZ7qAzE", json)
-        self.assertEqual(media.url, "https://www.youtube.com/watch?v=DJxchZ7qAzE")
-        self.assertEqual(media.type, ContentType.YOUTUBE)
-
+        param(
+            url="https://www.youtube.com/watch?v=DJxchZ7qAzE",
+            json={"media": {"oembed": {}}},
+            expected_url="https://www.youtube.com/watch?v=DJxchZ7qAzE"
+        ),
         # youtube link (youtube.com)
-        json = {
-            "media": {
-                "oembed": {
-                    "url": "https://www.youtube.com/watch?v=DJxchZ7qAzE"
-                }
-            }
-        }
-        media = ServicesWrapper.get_media("https://www.youtu.be/watch?v=DJxchZ7qAzE", json)
-        self.assertEqual(media.url, "https://www.youtube.com/watch?v=DJxchZ7qAzE")
+        param(
+            url="https://www.youtube.com/watch?v=DJxchZ7qAzE",
+            json={"media": {"oembed": {"url": "https://www.youtube.com/watch?v=DJxchZ7qAzE"}}},
+            expected_url="https://www.youtube.com/watch?v=DJxchZ7qAzE"
+        ),
+    ])
+    def test_youtube(self, url, json, expected_url):
+        media = ServicesWrapper.get_media(url, json)
+        self.assertEqual(media.url, expected_url)
         self.assertEqual(media.type, ContentType.YOUTUBE)
 
-    def test_imgur(self):
+    @parameterized.expand([
         # imgur gif
-        media = ServicesWrapper.get_media("https://i.imgur.com/2WNMUqO.gifv")
-        self.assertEqual(media.url, "https://i.imgur.com/2WNMUqO.mp4")
-        self.assertEqual(media.type, ContentType.VIDEO)
+        param(
+            url="https://i.imgur.com/2WNMUqO.gifv",
+            expected_url="https://i.imgur.com/2WNMUqO.mp4",
+            expected_type=ContentType.VIDEO
+        )
+    ])
+    def test_imgur(self, url, expected_url, expected_type):
+        media = ServicesWrapper.get_media(url)
+        self.assertEqual(media.url, expected_url)
+        self.assertEqual(media.type, expected_type)
 
-    def test_gfycat(self):
+    @parameterized.expand([
         # gfycat gif
-        media = ServicesWrapper.get_media("https://gfycat.com/HeartfeltHollowIberianchiffchaff")
-        self.assertEqual(media.url, "https://giant.gfycat.com/HeartfeltHollowIberianchiffchaff.mp4")
-        self.assertEqual(media.type, ContentType.VIDEO)
+        param(
+            url="https://gfycat.com/HeartfeltHollowIberianchiffchaff",
+            expected_url="https://giant.gfycat.com/HeartfeltHollowIberianchiffchaff.mp4",
+            expected_type=ContentType.VIDEO
+        )
+    ])
+    def test_gfycat(self, url, expected_url, expected_type):
+        media = ServicesWrapper.get_media(url)
+        self.assertEqual(media.url, expected_url)
+        self.assertEqual(media.type, expected_type)
 
-    def test_vreddit(self):
+    @parameterized.expand([
         # v.redd.it video
-        json = {
-            "media": {
-                "reddit_video": {
-                    "fallback_url": "https://v.redd.it/4phan5t9wq031/DASH_1080?source=fallback"
-                }
-            }
-        }
-        media = ServicesWrapper.get_media("https://v.redd.it/4phan5t9wq031", json)
-        self.assertEqual(media.url, "https://v.redd.it/4phan5t9wq031/DASH_1080?source=fallback")
-        self.assertEqual(media.type, ContentType.GIF)
+        param(
+            url="https://v.redd.it/4phan5t9wq031",
+            json={"media": {"reddit_video": {"fallback_url": "https://v.redd.it/4phan5t9wq031/DASH_1080?source=fallback"}}},
+            expected_url="https://v.redd.it/4phan5t9wq031/DASH_1080?source=fallback",
+            expected_type=ContentType.GIF
+        )
+    ])
+    def test_vreddit(self, url, json, expected_url, expected_type):
+        media = ServicesWrapper.get_media(url, json)
+        self.assertEqual(media.url, expected_url)
+        self.assertEqual(media.type, expected_type)
 
-    def test_generic(self):
+    @parameterized.expand([
         # i.redd.it image
-        media = ServicesWrapper.get_media("https://i.redd.it/72tqh40xqr031.jpg")
-        self.assertEqual(media.url, "https://i.redd.it/72tqh40xqr031.jpg")
-        self.assertEqual(media.type, ContentType.PHOTO)
-
+        param(
+            url="https://i.redd.it/72tqh40xqr031.jpg",
+            expected_url="https://i.redd.it/72tqh40xqr031.jpg",
+            expected_type=ContentType.PHOTO
+        ),
         # i.redd.it gif
-        media = ServicesWrapper.get_media("https://i.redd.it/ao5s72l50r031.gif")
-        self.assertEqual(media.url, "https://i.redd.it/ao5s72l50r031.gif")
-        self.assertEqual(media.type, ContentType.GIF)
+        param(
+            url="https://i.redd.it/ao5s72l50r031.gif",
+            expected_url="https://i.redd.it/ao5s72l50r031.gif",
+            expected_type=ContentType.GIF
+        )
+    ])
+    def test_generic(self, url, expected_url, expected_type):
+        media = ServicesWrapper.get_media(url)
+        self.assertEqual(media.url, expected_url)
+        self.assertEqual(media.type, expected_type)
